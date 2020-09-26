08c60de279c07e39e3493f81f39bcc7fa5e48caf
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2020-09-26 11:37:50 +0530

    Js_4

---
+++ js/payConfirm.js
@@ -0,0 +1,21 @@
+function PayPaypal() {
+    var txt;
+    var r = confirm("Do you Want to Pay via Paypal?");
+    if (r == true) {
+        
+    } else {
+        location.replace("Employer dashboard.html")
+    }
+    
+  }
+
+  function PayCard() {
+    var txt;
+    var r = confirm("Do you Want to Pay via Card?");
+    if (r == true) {
+        
+    } else {
+        location.replace("Employer dashboard.html")
+    }
+    
+  }
