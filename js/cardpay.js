8b36d3f3855b55dd64d2d57f1a9b64b9fc2955ae
GishanJ <gishanjayasundara@gmail.com>
2020-10-13 09:40:04 +0530

    cardpay

--- js/cardpay.js
+++ js/cardpay.js
@@ -1,4 +1,7 @@
-<form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
-  Name: <input type="text" name="fname">
-  <input type="submit" value="Submit">
-</form>
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
