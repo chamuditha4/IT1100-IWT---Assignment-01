db01b7af23d1b45a135bcd66356270030d267341
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2020-10-16 18:55:40 +0530

    Database Part

---
+++ V2/Xampp/htdocs/php/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../css/home.css">
+</head>
+<body>
+<div >
+      
+      <h1 id="today"> </h1>
+     
+     </div>
+     <nav>
+       <ul>
+         <li><a href="home.html">Home</a></li>
+         <li><a href="#">Job Portal</a>
+           <ul>
+             <li><a href="Login.html">Explore Jobs</a></li>
+             <li><a href="Login.html">Add Jobs</a></li>
+ 
+           </ul>
+         </li>
+         <li><a href="Map.html">Find Us</a></li>
+         <li><a href="about us.html" class="active">About Us</a>
+           <ul>
+             <li><a href="about%20us.html#aboutsec">Our Story</a></li>
+             <li><a href="about%20us.html#missionpa">Our Mission</a></li>
+             <li><a href="about%20us.html#wherewebac">Where We Work</a></li>
+           </ul>
+         </li>
+         <li><a href="contactUs.html">Contact Us</a></li>
+       </ul>
+     </nav>
+</body>
+</html>
