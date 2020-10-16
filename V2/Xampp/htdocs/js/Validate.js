0d1b857785d48f2043bcf913ee708c449b168bf2
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2020-10-16 03:54:32 +0000

    1

--- V2/Xampp/htdocs/js/Validate.js
+++
@@ -1,9 +0,0 @@
-function Validate() {
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("cpassword").value;
-    if (password != confirmPassword) {
-        alert("Passwords do not match.");
-        return false;
-    }
-    return true;
-}
--- V2/Xampp/htdocs/js/cardpay.js
+++
@@ -1,4 +0,0 @@
-function BOC() {
-    document.getElementById("p_image").src = "../images/aa.png";
-    document.getElementById("p_image").style.display = "block";
-    document.getElementById("bank").style.display = "none";
