15cda6c3b44a665f711e3c8ba269e12d399ec7ee
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2020-10-16 03:16:41 +0000

    V2

---
+++ V2/Xampp/cgi-bin/cgi.cgi
@@ -0,0 +1,5 @@
+#!"C:\Users\Administrator\Documents\IT1100-IWT---Assignment-01\V2\Xampp\perl\bin\perl.exe"
+
+print "Content-Type: text/html\n\n";
+
+print "OK";
---
+++ V2/Xampp/mysql_stop.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /D %~dp0
+echo Mysql shutdowm ...
+cmd.exe /C start "" /MIN call "C:\Users\Administrator\Documents\IT1100-IWT---Assignment-01\V2\Xampp\killprocess.bat" "mysqld.exe"
+
+if not exist mysql\data\%computername%.pid GOTO exit
+echo Delete %computername%.pid ...
+del mysql\data\%computername%.pid
+
+:exit
