5fab901710487b37554f8ded3c064bf87dd89155
Capsy <71144025+Capsy001@users.noreply.github.com>
2020-10-17 21:20:29 +0530

    Capsy V2.5

--- V2/Xampp/htdocs/php/home.php
+++ V2/Xampp/htdocs/php/home.php
@@ -2,11 +2,36 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Online Job Portal</title>
+  <title>Online Job Portal</title>
+  <link rel="stylesheet" href="../css/home.css">
   <script type="text/javascript" src="../js/slide.js"></script>
 </head>
 <body onload="showSlides()">
-    <?php include 'header.php';?>
+    
+<nav>
+       <ul>
+         <li><a href="home.php">Home</a></li>
+         <li><a href="#">Job Portal</a>
+           <ul>
+             <li><a href="Login.html">Explore Jobs</a></li>
+             <li><a href="Login.html">Add Jobs</a></li>
+ 
+           </ul>
+         </li>
+         <li><a href="Map.html">Find Us</a></li>
+         <li><a href="about us.html">About Us</a>
+           <ul>
+             <li><a href="about%20us.html#aboutsec">Our Story</a></li>
+             <li><a href="about%20us.html#missionpa">Our Mission</a></li>
+             <li><a href="about%20us.html#wherewebac">Where We Work</a></li>
+           </ul>
+         </li>
+         <li><a href="contactUs.html">Contact Us</a></li>
+       </ul>
+     </nav>
+
+
+
     <img src="../images/White logo m.png" id="logo">
 
     <div class="home">
@@ -17,32 +42,34 @@
 
     <div class="slideshow-container" >
 
-      <div class="mySlides">
-        <div class="numbertext">1 / 3</div>
-        <img src="../images/slide1.jpg" style="width:100%">
-        <div class="text">MAKE MONEY</div>
-      </div>
-
-      <div class="mySlides">
-        <div class="numbertext">2 / 3</div>
-        <img src="../images/slide2.jpg" style="width:100%">
-        <div class="text">SEARCH JOBS</div>
-      </div>
-
-      <div class="mySlides">
-        <div class="numbertext">3 / 3</div>
-        <img src="../images/slide3.jpg" style="width:100%">
-        <div class="text">WORK FROM HOME</div>
-      </div>
-
-    </div>
+                                  <div class="mySlides">
+                                    <div class="numbertext">1 / 3</div>
+                                    <img src="../images/slide1.jpg" style="width:100%">
+                                    <div class="text">MAKE MONEY</div>
+                                  </div>
+                                  
+                                  <div class="mySlides">
+                                    <div class="numbertext">2 / 3</div>
+                                    <img src="../images/slide2.jpg" style="width:100%">
+                                    <div class="text">SEARCH JOBS</div>
+                                  </div>
+                                  
+                                  <div class="mySlides">
+                                    <div class="numbertext">3 / 3</div>
+                                    <img src="../images/slide3.jpg" style="width:100%">
+                                    <div class="text">WORK FROM HOME</div>
+                                  </div>
+                                  
+                                  </div>
+                                  <br>
+                                  
+                                  <div style="text-align:center">
+                                    <span class="dot"></span> 
+                                    <span class="dot"></span> 
+                                    <span class="dot"></span> 
+                                  </div>
 <br>
 
-<div style="text-align:center">
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-</div>
 
 
     <?php include 'footer.php';?>
