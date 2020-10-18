e66cc4b121c2698370e1c8ccba9661e179ab6fa8
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2020-10-18 11:34:10 +0530

    ADD VCA - V@ Bug 2 FIXED

--- V2/Xampp/htdocs/php/Vauth.php
+++ V2/Xampp/htdocs/php/Vauth.php
@@ -4,21 +4,19 @@ $title = "";
 $descrption = "";
 $skill = "";
 $payment = "";
-$uid = "" ;
+$uname = $_SESSION['username'];
+$sql = "SELECT * FROM users WHERE username='$uname'";
+$result = $db->query($sql);
+$row = $result->fetch_assoc();
+$uid = $row["id"];
 if (isset($_POST['AddVacancy'])) {
-    $uname = $_SESSION['username'];
-    $sql = "SELECT * FROM users WHERE username='$uname'";
-    $result = $db->query($sql);
-    $row = $result->fetch_assoc();
+    
     
     $title = mysqli_real_escape_string($db, $_POST['title']);
     $descrption = mysqli_real_escape_string($db, $_POST['des']);
     $skill = mysqli_real_escape_string($db, $_POST['skills']);
     $payment = mysqli_real_escape_string($db, $_POST['pyment']);
-    $uid = $row["id"];
-    // Finally, register user if there are no errors in the form
-
-  
+    
         $query = "INSERT INTO vacancies (uid, title, description, payment, skills) 
                   VALUES('$uid', '$title', '$descrption', '$payment', '$skill')";
         mysqli_query($db, $query);
