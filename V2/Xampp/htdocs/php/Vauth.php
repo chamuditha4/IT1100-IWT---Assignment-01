b8b73dbfa7930fbf02c525bb72d79fc320b9c428
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2020-10-18 11:36:19 +0530

    12

--- V2/Xampp/htdocs/php/Vauth.php
+++ V2/Xampp/htdocs/php/Vauth.php
@@ -9,6 +9,7 @@ $sql = "SELECT * FROM users WHERE username='$uname'";
 $result = $db->query($sql);
 $row = $result->fetch_assoc();
 $uid = $row["id"];
+// START ADD Vacancies
 if (isset($_POST['AddVacancy'])) {
     
     
@@ -16,12 +17,12 @@ if (isset($_POST['AddVacancy'])) {
     $descrption = mysqli_real_escape_string($db, $_POST['des']);
     $skill = mysqli_real_escape_string($db, $_POST['skills']);
     $payment = mysqli_real_escape_string($db, $_POST['pyment']);
-    
+
         $query = "INSERT INTO vacancies (uid, title, description, payment, skills) 
                   VALUES('$uid', '$title', '$descrption', '$payment', '$skill')";
         mysqli_query($db, $query);
         header('location: Edash.php');
   }
-
+// END ADD Vacancies
 
 ?>
